d309c2a10bbc8d22db52a9f476b3bc6399e35c99
Emaltard <maxence.joulin@gmail.com>
2018-04-13 19:59:38 +0200

    add blur on one color

--- TP4/main.c
+++ TP4/main.c
@@ -63,6 +63,52 @@ void sequential_blur(int width, int_bmp_pixel_t (*tab)[width]){
         Liberation_image_lue_onemalloc(temp);
 }
 
+void sequential_blur_one_color(int width, int_bmp_pixel_t (*tab)[width], int color){
+        int_bmp_pixel_t (*temp)[width] = (int_bmp_pixel_t (*)[width])(int_bmp_pixel_t *) malloc((width)*(get_img_heigh())*sizeof(int_bmp_pixel_t));
+
+        for(int i=0; i<get_img_heigh(); i++) {
+                for(int j=0; j<width; j++) {
+                        temp[i][j] = tab[i][j];
+                }
+        }
+
+        for(int i=0; i<get_img_heigh(); i++) {
+                for(int j=0; j<width; j++) {
+                        int nbr_elem_for_blur = 0;
+                        for(int k=i-1; k<=i+1; k++) {
+                                for(int l=j-1; l<=j+1; l++) {
+                                        if((k>=0) && (k<width) && (l>=0) && (l<get_img_heigh())) {
+                                                if(!(k==i && l==j)) {
+                                                        nbr_elem_for_blur++;
+                                                        if(color==0 || color==1) {
+                                                                tab[i][j].Rouge += temp[k][l].Rouge;
+                                                        }
+                                                        if(color == 0 || color==2) {
+                                                                tab[i][j].Bleu += temp[k][l].Bleu;
+                                                        }
+                                                        if(color == 0 || color==3) {
+                                                                tab[i][j].Vert += temp[k][l].Vert;
+                                                        }
+                                                }
+                                        }
+                                }
+                        }
+
+                        if(color==0 || color==1) {
+                                tab[i][j].Rouge = tab[i][j].Rouge / nbr_elem_for_blur;
+                        }
+                        if(color == 0 || color==2) {
+                                tab[i][j].Bleu = tab[i][j].Bleu / nbr_elem_for_blur;
+                        }
+                        if(color == 0 || color==3) {
+                                tab[i][j].Vert = tab[i][j].Vert / nbr_elem_for_blur;
+                        }
+                }
+        }
+
+        Liberation_image_lue_onemalloc(temp);
+}
+
 void parallel_vertical_symmetry(int width, int_bmp_pixel_t (*tab)[width], int nbproc){
 
         int_bmp_pixel_t (*rcv)[width] = (int_bmp_pixel_t (*)[width])(int_bmp_pixel_t *) malloc((width)*(get_img_heigh())*sizeof(int_bmp_pixel_t));
@@ -146,7 +192,8 @@ int main(int argc, char* argv[])
 
         if(rank==0) {
                 int_bmp_pixel_t (*tab2)[width] = Lecture_image_onemalloc("pingouin.bmp");
-                sequential_blur(width, tab2);
+                //sequential_blur(width, tab2);
+                sequential_blur_one_color(width, tab2, 0);
                 printf("%d %d\n", get_img_width(), get_img_heigh());
                 Ecriture_image_onemalloc(tab, "copie.bmp");
                 Ecriture_image_onemalloc(tab2, "copie2.bmp");
